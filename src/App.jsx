c84496d998fb99e70d3a4712dab7400ddf5bb074
rainathecreatour-ops <rainathecreatour@gmail.com>
2026-02-05 10:17:46 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Download, Lock } from 'lucide-react'; 
-
+ 
 const PlannerGenerator = () => {
  const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [accessCode, setAccessCode] = useState('');
